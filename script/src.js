8547797fed0312c960d81a0d6deae3054c75439a
Stevierajesh <78890359+Stevierajesh@users.noreply.github.com>
2023-10-28 19:22:29 -0400

    Update 4
    Keep working

--- script/src.js
+++ script/src.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from firebase/app;
+import { getAnalytics } from firebase/analytics;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,10 +13,20 @@ const firebaseConfig = {
   projectId: "csproj1-b4de7",
   storageBucket: "csproj1-b4de7.appspot.com",
   messagingSenderId: "445466382978",
-  appId: "1:445466382978:web:864132255c8cd147cc9a9b",
-  measurementId: "G-ZBPLMV9ME5"
+  appId: "1:445466382978:web:7522acdcf12fd399cc9a9b",
+  measurementId: "G-6KKC3XYB4T"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+var database = firebase.database();
+  var ref = database.ref('/Accounts/Admin');
+
+  ref.on('value', function(snapshot) {
+    var data = snapshot.val();
+    // document.getElementById("your-element-id").innerHTML = data;
+    console.log(data);
+    console.log("lol");
+  });
